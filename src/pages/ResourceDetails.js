15be1083b0c85cc2eba54586f3c9bc972290d43b
admin <igbagboleye@gmail.com>
2023-03-31 06:57:02 +0100

    resources details fixed

--- src/pages/ResourceDetails.js
+++ src/pages/ResourceDetails.js
@@ -1,38 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import pattern from "../assets/images/pattern.jpg";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { headings } from "../utils/resourcesData";
 import { BsFileEarmarkPdfFill } from "react-icons/bs";
 import { RiPagesLine } from "react-icons/ri";
 
-const ResourceDetails = () => {
+const ResourceDetails = (props) => {
+  //   const userId = props.match.params.id;
+  const { id } = useParams();
+
+  console.log(id);
+
+  const [itemActive, setItemActive] = useState(id - 1);
+  const [item, setItem] = useState(headings[itemActive]);
+
+  useEffect(() => {
+    setItemActive(id - 1);
+    let item = headings[id - 1];
+    console.log("hello");
+    setItem(item);
+  }, [id]);
+
+  useEffect(() => {
+    const el = document.getElementById("resources");
+
+    const { top } = el.getBoundingClientRect();
+    const scrolledY = window.scrollY;
+
+    const position = top + scrolledY - 50;
+
+    window.scroll(0, position);
+  }, []);
+
   return (
     <div className="resource-details">
       <section
         className="page-header"
         style={{ backgroundImage: `url(${pattern})` }}
       >
-        <h1>Expand SEO</h1>
+        <h1>{item.text}</h1>
         <p>
           <Link to="/">Home</Link> / <Link to="/resources">Resources</Link> /
-          Expand SEO
+          {item.text}
         </p>
       </section>
-      <div className="section-two">
+      <div className="section-two" id="resources">
         <div className="content-center">
           <div className="content">
             <div className="right">
-              {headings.map((item) => {
+              {headings.map((item, index) => {
                 const { text, icon, id } = item;
 
                 return (
-                  <div
+                  <Link
                     key={id}
-                    className={`${id === 2 ? "item active" : "item"}`}
+                    className={`${
+                      index === itemActive ? "item active" : "item"
+                    }`}
+                    // onClick={() => setItemActive(index)}
+                    to={`/resources/${id}`}
                   >
                     <span>{icon}</span>
                     <h2>{text}</h2>
-                  </div>
+                  </Link>
                 );
               })}
             </div>
@@ -41,7 +72,7 @@ const ResourceDetails = () => {
                 <p>all</p> <span>50</span>
               </div>
               <div className="bottom">
-                {headings[0].items.map((i, index) => {
+                {headings[itemActive].items.map((i, index) => {
                   const { head, text } = i;
                   return (
                     <div key={index} className="item">
--- src/utils/resourcesData.js
+++ src/utils/resourcesData.js
@@ -48,25 +48,195 @@ export const headings = [
     id: 2,
     text: "expand SEO",
     icon: <TbSeo />,
+    items: [
+      {
+        head: "Expand development",
+        text: "676 pages",
+      },
+      {
+        head: "expand design",
+        text: "616 pages",
+      },
+      {
+        head: "expand page",
+        text: "643 pages",
+      },
+      {
+        head: "business compare",
+        text: "216 pages",
+      },
+      {
+        head: "comerce checkout",
+        text: "706 pages",
+      },
+      {
+        head: "data staging",
+        text: "73 pages",
+      },
+      {
+        head: "campaign store",
+        text: "2976 pages",
+      },
+      {
+        head: "acquisition mitra",
+        text: "376 pages",
+      },
+    ],
   },
   {
     id: 3,
     text: "office management",
     icon: <HiOutlineOfficeBuilding />,
+    items: [
+      {
+        head: "office development",
+        text: "756 pages",
+      },
+      {
+        head: "office design",
+        text: "196 pages",
+      },
+      {
+        head: "office page",
+        text: "943 pages",
+      },
+      {
+        head: "office compare",
+        text: "216 pages",
+      },
+      {
+        head: "comerce checkout",
+        text: "706 pages",
+      },
+      {
+        head: "data staging",
+        text: "73 pages",
+      },
+      {
+        head: "campaign store",
+        text: "2976 pages",
+      },
+      {
+        head: "acquisition mitra",
+        text: "376 pages",
+      },
+    ],
   },
   {
     id: 4,
     text: "client managing",
     icon: <SiOnlyoffice />,
+    items: [
+      {
+        head: "client development",
+        text: "776 pages",
+      },
+      {
+        head: "client design",
+        text: "166 pages",
+      },
+      {
+        head: "client page",
+        text: "343 pages",
+      },
+      {
+        head: "client compare",
+        text: "216 pages",
+      },
+      {
+        head: "comerce checkout",
+        text: "706 pages",
+      },
+      {
+        head: "data staging",
+        text: "73 pages",
+      },
+      {
+        head: "campaign store",
+        text: "2976 pages",
+      },
+      {
+        head: "acquisition mitra",
+        text: "376 pages",
+      },
+    ],
   },
   {
     id: 5,
     text: "data collection",
     icon: <BsDatabaseFillCheck />,
+    items: [
+      {
+        head: "data development",
+        text: "176 pages",
+      },
+      {
+        head: "data design",
+        text: "136 pages",
+      },
+      {
+        head: "data page",
+        text: "435 pages",
+      },
+      {
+        head: "data compare",
+        text: "216 pages",
+      },
+      {
+        head: "comerce checkout",
+        text: "706 pages",
+      },
+      {
+        head: "data staging",
+        text: "73 pages",
+      },
+      {
+        head: "campaign store",
+        text: "2976 pages",
+      },
+      {
+        head: "acquisition mitra",
+        text: "376 pages",
+      },
+    ],
   },
   {
     id: 6,
     text: "secured system",
     icon: <SiSpringsecurity />,
+    items: [
+      {
+        head: "secured development",
+        text: "761 pages",
+      },
+      {
+        head: "secured design",
+        text: "106 pages",
+      },
+      {
+        head: "secured page",
+        text: "432 pages",
+      },
+      {
+        head: "secured compare",
+        text: "216 pages",
+      },
+      {
+        head: "comerce checkout",
+        text: "706 pages",
+      },
+      {
+        head: "data staging",
+        text: "73 pages",
+      },
+      {
+        head: "campaign store",
+        text: "2976 pages",
+      },
+      {
+        head: "acquisition mitra",
+        text: "376 pages",
+      },
+    ],
   },
 ];
