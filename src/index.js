22ad9ab8ff5034519bd4f1fe0f923276d15e18d5
igee-17 <igbagboleye@gmail.com>
2023-07-24 13:06:59 +0100

    new changes for refresh

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import "./styles/index.scss";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 // FIREBASE
 // Import the functions you need from the SDKs you need
